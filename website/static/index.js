0215af5f96b03ffcf99537f6be6af4a23c2541c6
AlexValdez85 <alejandrovaldez802@gmail.com>
2023-06-30 09:25:34 -0600

    inicio dinamico

---
+++ website/static/index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var elementos = document.getElementsByClassName("miElemento");
+    for (var i = 0; i < 3; i++) {
+        elementos[i].style.display = "bloc";
+    }
+};
